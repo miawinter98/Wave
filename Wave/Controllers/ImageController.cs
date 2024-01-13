f26f961e7991296a54b257314760783ce6739371
Mia Winter <mia@miawinter.de>
2024-01-13 15:30:34 +0100

    Added simple support for image upload and requesting

---
+++ Wave/Controllers/ImageController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Wave.Services;
+
+namespace Wave.Controllers;
+
+[ApiController]
+[Route("/images")]
+public class ImageController(ImageService imageService) : ControllerBase {
+    private ImageService ImageService { get; } = imageService;
+
+    [HttpGet]
+    [Route("{imageId:guid}")]
+    public IActionResult Get(Guid imageId) {
+        string? path = ImageService.GetPath(imageId);
+
+        if (path is null) return NotFound();
+        return File(System.IO.File.OpenRead(path), ImageService.ImageMimeType);
+    }
+}
---
+++ Wave/Services/ImageService.cs
@@ -0,0 +1,50 @@
+﻿using ImageMagick;
+
+namespace Wave.Services;
+
+public class ImageService(ILogger<ImageService> logger) {
+    private ILogger<ImageService> Logger { get; } = logger;
+    private const string BasePath = "./files/images";
+    private const string ImageExtension = ".jpg";
+    public string ImageMimeType => "image/jpg";
+
+    public string? GetPath(Guid imageId) {
+        string path = Path.Combine(BasePath, imageId + ImageExtension);
+        return File.Exists(path) ? path : null;
+    }
+
+    public async ValueTask<Guid?> StoreImageAsync(string temporaryPath, int size = 800, CancellationToken cancellation = default) {
+        if (File.Exists(temporaryPath) is not true) return null;
+
+        try {
+            if (File.Exists(BasePath) is not true) Directory.CreateDirectory(BasePath);
+
+            var image = new MagickImage();
+            await image.ReadAsync(temporaryPath, cancellation);
+
+            // Jpeg with 90% compression should look decent
+            image.Resize(new MagickGeometry(size)); // this preserves aspect ratio
+            image.Format = MagickFormat.Jpeg;
+            image.Quality = 90;
+
+            if (image.GetExifProfile() is not null) {
+                image.RemoveProfile(image.GetExifProfile());
+            }
+
+            // Overwrite exif for privacy reasons
+            var exif = new ExifProfile {
+                Parts = ExifParts.None
+            };
+            exif.CreateThumbnail();
+            image.SetProfile(exif);
+            
+            var guid = Guid.NewGuid();
+            string path = Path.Combine(BasePath, guid + ImageExtension);
+            await image.WriteAsync(path, cancellation);
+            return guid;
+        } catch (Exception ex) {
+            Logger.LogInformation(ex, "Failed to process uploaded image.");
+            return null;
+        }
+    }
+}
