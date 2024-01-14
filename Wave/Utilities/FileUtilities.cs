7b636a28df18f9ddc6ba7e59d7362ade68b7d643
Mia Winter <mia@miawinter.de>
2024-01-14 13:00:16 +0100

    Added FileUtilities for user uploaded files

---
+++ Wave/Utilities/FileUtilities.cs
@@ -0,0 +1,15 @@
+﻿namespace Wave.Utilities;
+
+public static class FileUtilities {
+    public static async Task<string> StoreTemporary(Stream fileStream) {
+        string tempName = Path.GetRandomFileName();
+        string tempDirectory = Path.Combine(".", "files", "temp");
+        Directory.CreateDirectory(tempDirectory);
+        string tempPath = Path.Combine(tempDirectory, tempName);
+        await using var fs = new FileStream(tempPath, FileMode.Create);
+        
+        await fileStream.CopyToAsync(fs);
+
+        return tempPath;
+    }
+}
