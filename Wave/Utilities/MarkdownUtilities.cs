3d966ac2ddfa6194c87a3d5b791e024ab75f2e4b
Mia Winter <mia@miawinter.de>
2024-01-17 14:04:59 +0100

    Moved markdown parsing to separate function

---
+++ Wave/Utilities/MarkdownUtilities.cs
@@ -0,0 +1,15 @@
+﻿using Markdig;
+using Wave.Data;
+
+namespace Wave.Utilities;
+
+public static class MarkdownUtilities {
+    public static string Parse(string markdown) {
+        var pipeline = new MarkdownPipelineBuilder()
+            .UsePipeTables()
+            .UseEmphasisExtras()
+            .DisableHtml()
+		.Build();
+		return Markdown.ToHtml(markdown, pipeline);
+    }
+}
