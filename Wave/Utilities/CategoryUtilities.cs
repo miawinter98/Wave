df7c7c7534bf8989446bb9d8befe0c64fdaec08c
Mia Winter <mia@miawinter.de>
2024-01-26 13:06:03 +0100

    Added Category Manage Page

---
+++ Wave/Utilities/CategoryUtilities.cs
@@ -0,0 +1,18 @@
+﻿using Wave.Data;
+
+namespace Wave.Utilities;
+
+public static class CategoryUtilities {
+    public static string GetCssClassPostfixForColor(CategoryColors color) {
+        return color switch {
+            CategoryColors.Primary => "primary",
+            CategoryColors.Dangerous => "error",
+            CategoryColors.Important => "warning",
+            CategoryColors.Informative => "info",
+            CategoryColors.Secondary => "secondary",
+            CategoryColors.Default => "",
+            CategoryColors.Extra => "accent",
+            _ => throw new ArgumentOutOfRangeException(nameof(color), color, null)
+        };
+    }
+}
