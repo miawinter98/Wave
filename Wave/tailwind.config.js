21dad2f7285f8bdfad8d98ea2096c9cfab24d5da
Mia Winter <mia@miawinter.de>
2024-02-03 21:17:47 +0100

    improved youtube embeds

--- Wave/tailwind.config.js
+++ Wave/tailwind.config.js
@@ -3,6 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
     content: ["Pages/**/*.cshtml", "Components/**/*.razor"],
+    safelist: ["youtube"],
     theme: {
         extend: {
         },
