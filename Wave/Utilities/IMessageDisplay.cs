33e8d490ac88f747d7d5ade2588e94e644056b82
Mia Winter <mia@miawinter.de>
2024-02-14 19:41:26 +0100

    Added IMessageDisplay Interface, added implementation to ToastComponent

---
+++ Wave/Utilities/IMessageDisplay.cs
@@ -0,0 +1,16 @@
+﻿namespace Wave.Utilities;
+
+public interface IMessageDisplay {
+	public void ShowMessage(Message message);
+	
+	public void ShowInfo(string message, string? title = null)
+		=> ShowMessage(new Message(message, "alert-info", title, DateTimeOffset.UtcNow));
+	public void ShowSuccess(string message, string? title = null)
+		=> ShowMessage(new Message(message, "alert-success", title, DateTimeOffset.UtcNow));
+	public void ShowWarning(string message, string? title = null)
+		=> ShowMessage(new Message(message, "alert-warning", title, DateTimeOffset.UtcNow));
+	public void ShowError(string message, string? title = null)
+		=> ShowMessage(new Message(message, "alert-error", title, DateTimeOffset.UtcNow));
+
+	public sealed record Message(string Body, string Type, string? Title, DateTimeOffset Created);
+}
