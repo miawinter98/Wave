55934955cc4b1f46e132cda5add7dfd28b4984f0
Mia Winter <mia@miawinter.de>
2024-02-18 19:31:17 +0100

    Added contact information to user

--- Wave/Data/ApplicationUser.cs
+++ Wave/Data/ApplicationUser.cs
@@ -13,10 +13,21 @@ public class ApplicationUser : IdentityUser {
     public string AboutTheAuthor { get; set; } = string.Empty;
     [MaxLength(4096), PersonalData]
     public string Biography { get; set; } = string.Empty;
-    public string BiographyHtml { get; set; } = string.Empty;
+	// ReSharper disable once EntityFramework.ModelValidation.UnlimitedStringLength
+	public string BiographyHtml { get; set; } = string.Empty;
 
     public string Name => FullName ?? "Guest Author";
 
     public IList<Article> Articles { get; set; } = [];
+    [PersonalData]
     public IList<UserLink> Links { get; set; } = [];
+
+    [MaxLength(128), EmailAddress, PersonalData]
+	public string ContactEmail { get; set; } = string.Empty;
+	[MaxLength(64), Phone, PersonalData]
+	public string ContactPhone { get; set; } = string.Empty;
+	[MaxLength(64), Phone, PersonalData]
+	public string ContactPhoneBusiness { get; set; } = string.Empty;
+	[MaxLength(128), Phone, PersonalData]
+	public string ContactWebsite { get; set; } = string.Empty;
 }
---
+++ Wave/Data/Migrations/postgres/20240218183023_UserContactInformation.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wave.Data.Migrations.postgres
+{
+    /// <inheritdoc />
+    public partial class UserContactInformation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactEmail",
+                table: "AspNetUsers",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPhone",
+                table: "AspNetUsers",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactPhoneBusiness",
+                table: "AspNetUsers",
+                type: "character varying(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ContactWebsite",
+                table: "AspNetUsers",
+                type: "character varying(128)",
+                maxLength: 128,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactEmail",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ContactPhone",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ContactPhoneBusiness",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ContactWebsite",
+                table: "AspNetUsers");
+        }
+    }
+}
