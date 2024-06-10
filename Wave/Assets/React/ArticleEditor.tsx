ed770ed783d4986eec2fda16e35a57e31e3297a1
Mia Winter <mia@miawinter.de>
2024-06-11 00:47:40 +0200

    added article status steps to react component noticed I need to figure out client side localization

--- Wave/Assets/React/ArticleEditor.tsx
+++ Wave/Assets/React/ArticleEditor.tsx
@@ -1,7 +1,20 @@
 import "vite/modulepreload-polyfill";
+import { useState } from 'react';
 
 function Editor() {
-    return <h2>Hello from React!</h2>;
+    const status = "draft";
+
+    return (
+        <>
+            <div className="w-full">
+                <ul className="steps w-full max-w-xs">
+                    <li className={`step ${status === "draft" ? "step-secondary" : ""}`}>@Localizer["Draft"]</li>
+                    <li className={`step ${status === "in_review" ? "step-secondary" : ""}`}>@Localizer["InReview"]</li>
+                    <li className={`step ${status === "published" ? "step-secondary" : ""}`}>@Localizer["Published"]</li>
+                </ul>
+            </div>
+        </>
+    );
 }
 
 export default Editor;
