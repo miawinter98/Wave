ecb766b0305b792a6dcb693c4600c89876106645
Mia Rose Winter <mia@miawinter.de>
2024-12-30 20:09:11 +0100

    Implemented fediverse:creator meta tag with profile setting

--- Wave/Data/ApplicationUser.cs
+++ Wave/Data/ApplicationUser.cs
@@ -4,25 +4,25 @@ using Microsoft.AspNetCore.Identity;
 namespace Wave.Data;
 
 public class ApplicationUser : IdentityUser {
-    public ProfilePicture? ProfilePicture { get; set; }
+	public ProfilePicture? ProfilePicture { get; set; }
 
-    [MaxLength(64), PersonalData]
-    public string? FullName { get; set; }
+	[MaxLength(64), PersonalData]
+	public string? FullName { get; set; }
 
-    [MaxLength(512), PersonalData]
-    public string AboutTheAuthor { get; set; } = string.Empty;
-    [MaxLength(4096), PersonalData]
-    public string Biography { get; set; } = string.Empty;
+	[MaxLength(512), PersonalData]
+	public string AboutTheAuthor { get; set; } = string.Empty;
+	[MaxLength(4096), PersonalData]
+	public string Biography { get; set; } = string.Empty;
 	// ReSharper disable once EntityFramework.ModelValidation.UnlimitedStringLength
 	public string BiographyHtml { get; set; } = string.Empty;
 
-    public string Name => FullName ?? "Guest Author";
+	public string Name => FullName ?? "Guest Author";
 
-    public IList<Article> Articles { get; set; } = [];
-    [PersonalData]
-    public IList<UserLink> Links { get; set; } = [];
+	public IList<Article> Articles { get; set; } = [];
+	[PersonalData]
+	public IList<UserLink> Links { get; set; } = [];
 
-    [MaxLength(128), EmailAddress, PersonalData]
+	[MaxLength(128), EmailAddress, PersonalData]
 	public string ContactEmail { get; set; } = string.Empty;
 	[MaxLength(64), Phone, PersonalData]
 	public string ContactPhone { get; set; } = string.Empty;
@@ -30,4 +30,6 @@ public class ApplicationUser : IdentityUser {
 	public string ContactPhoneBusiness { get; set; } = string.Empty;
 	[MaxLength(128), Url, PersonalData]
 	public string ContactWebsite { get; set; } = string.Empty;
+	[MaxLength(4096), RegularExpression(@"@.+@.+\..+"), PersonalData]
+	public string ContactFediverse { get; set; } = string.Empty;
 }
---
+++ Wave/Data/Migrations/postgres/20241230142359_UserContactFediverse.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wave.Data.Migrations.postgres;
+
+/// <inheritdoc />
+public partial class UserContactFediverse : Migration {
+	/// <inheritdoc />
+	protected override void Up(MigrationBuilder migrationBuilder) {
+		migrationBuilder.AddColumn<string>(
+			name: "ContactFediverse",
+			table: "AspNetUsers",
+			type: "character varying(4096)",
+			maxLength: 4096,
+			nullable: false,
+			defaultValue: "");
+	}
+
+	/// <inheritdoc />
+	protected override void Down(MigrationBuilder migrationBuilder) {
+		migrationBuilder.DropColumn(
+			name: "ContactFediverse",
+			table: "AspNetUsers");
+	}
+}
